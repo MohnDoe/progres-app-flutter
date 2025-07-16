058c5d1d82a5abfa43e894f27a4ce104c9b610a8
MohnDoe <kfnpfeffer@gmail.com>
2025-07-16 13:39:49 +0200

    feat: create component yay

---
+++ lib/src/core/ui/widgets/picture_rectangle.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:progres/src/core/domain/models/progress_picture.dart';
+
+class PictureRectangle extends StatelessWidget {
+  const PictureRectangle(this.picture, {super.key});
+
+  final ProgressPicture picture;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
