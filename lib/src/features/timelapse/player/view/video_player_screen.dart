a0b3b89bf8e4f379a4331bed19974797f4d862e0
MohnDoe <kfnpfeffer@gmail.com>
2025-07-18 14:52:35 +0200

    Basic control

--- lib/src/features/timelapse/player/view/video_player_screen.dart
+++ lib/src/features/timelapse/player/view/video_player_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:progres/src/core/services/video_service.dart';
 import 'package:progres/src/core/ui/widgets/picture_rectangle.dart';
@@ -61,20 +62,91 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            _controller.value.isInitialized
-                ? Material(
-                    child: PictureRectangle(
-                      null,
-                      width: double.infinity,
-                      height: double.infinity,
-                      borderRadius: 64,
-                      highlightColor: Theme.of(context).colorScheme.primary,
-                      highlight: true,
-                      aspectRatio: _controller.value.aspectRatio,
-                      emptyWidget: VideoPlayer(_controller),
-                    ),
-                  )
-                : const CircularProgressIndicator(),
+            PictureRectangle(
+              null,
+              width: double.infinity,
+              height: double.infinity,
+              borderRadius: 64,
+              highlightColor: Theme.of(context).colorScheme.primary,
+              highlight: true,
+              aspectRatio: _controller.value.aspectRatio,
+              emptyWidget: _controller.value.isInitialized
+                  ? VideoPlayer(_controller)
+                  : const Center(child: CircularProgressIndicator()),
+            ),
+            const SizedBox(height: 32),
+            VideoProgressIndicator(
+              _controller,
+              allowScrubbing: true,
+              colors: VideoProgressColors(
+                playedColor: Theme.of(context).colorScheme.primary,
+                bufferedColor: Theme.of(
+                  context,
+                ).colorScheme.surfaceContainerHighest,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 16,
+              children: [
+                IconButton.filledTonal(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
+                  ),
+                  onPressed: () {
+                    _controller.seekTo(Duration.zero);
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.backwardFast),
+                ),
+                IconButton.filled(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 16,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (_controller.value.isPlaying) {
+                        _controller.pause();
+                      } else {
+                        _controller.play();
+                      }
+                    });
+                  },
+                  icon: FaIcon(
+                    _controller.value.isPlaying
+                        ? FontAwesomeIcons.pause
+                        : FontAwesomeIcons.play,
+                  ),
+                ),
+                IconButton.filledTonal(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
+                  ),
+                  style: _controller.value.isLooping
+                      ? IconButton.styleFrom(
+                          backgroundColor: Theme.of(
+                            context,
+                          ).colorScheme.primary,
+                        )
+                      : null,
+                  onPressed: () {
+                    setState(() {
+                      _controller.setLooping(!_controller.value.isLooping);
+                    });
+                  },
+                  icon: FaIcon(
+                    FontAwesomeIcons.repeat,
+                    color: _controller.value.isLooping
+                        ? Theme.of(context).colorScheme.onPrimary
+                        : null,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
